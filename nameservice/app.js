7f63cb2b0d74991db6da395194184e82a082bca3
Perris Davis <wikipediabrown@users.noreply.github.com>
2017-08-18 02:02:15 -0700

    Added one GRPC service

--- nameservice/app.js
+++ nameservice/app.js
@@ -1,16 +1,34 @@
 'use strict';
 
-const express = require('express');
+const grpc = require('grpc');
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+// Constants, credentials and proto files
+const names_proto = grpc.load('./information.proto').information;
+const credential = grpc.ServerCredentials.createInsecure()
+const localhost =  '0.0.0.0:'
+const port = (process.env.PORT || '8080')
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello Capital One\n');
-});
+// RPC functions
+const getNames = function(call) {
+	console.log('GetNames Called')
+	call.write()
+	call.end
+}
 
-app.listen(PORT, HOST);
-console.log(`Running at http://${HOST}:${PORT}`);
+
+// Create GRPC Server
+var grpcServer = grpc.Server();
+
+// Prepair Names service and register names functions
+const namesService = names_proto.names.service;
+const namesFunctions = {getNames: getNames}
+
+// add service and functions to server
+grpcServer.addService(namesService, namesFunctions);
+
+// Bind server to port and set credentials
+grpcServer.bind(localhost+port, credentials)
+
+// Start server
+grpcServer.start();
+console.log('Server is listending at http:${localhost}:${port}');
